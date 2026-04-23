41d44c9719ec54341757176a687db113055f08c1
MarDom <113973897+marinehosch@users.noreply.github.com>
2026-04-23 15:50:39 +0200

    Update Vite config to include proj4 alias
    Added alias for proj4 to use pre-compiled version.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import dsv from '@rollup/plugin-dsv'
 
 export default defineConfig({
-    plugins: [
-        dsv(),
-],
-    build: {
-        minify: false
+  plugins: [
+    dsv(),
+  ],
+  resolve: {
+    alias: {
+      // Dit à Vite d'utiliser la version pré-compilée de proj4
+      'proj4': 'proj4/dist/proj4.js' 
     }
+  }
 })
